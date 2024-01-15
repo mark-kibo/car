1470204cc38c96997f985baf2efbd5736fd1d8a5
mark kibocha <63925315+mark-kibo@users.noreply.github.com>
2024-01-16 01:20:19 +0300

    Update settings.py

--- cardealer/settings.py
+++ cardealer/settings.py
@@ -96,14 +96,18 @@ WSGI_APPLICATION = 'cardealer.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cardealer_db',
-        'USER': 'postgres', 
-        'PASSWORD': '@Bryt567oN', 
-        'HOST': '127.0.0.1',
-		'PORT': '5432',
-     }
+  #    'default': {
+  #       'ENGINE': 'django.db.backends.postgresql',
+  #       'NAME': 'cardealer_db',
+  #       'USER': 'postgres', 
+  #       'PASSWORD': '@Bryt567oN', 
+  #       'HOST': '127.0.0.1',
+		# 'PORT': '5432',
+  #    }
+	'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
  }
 
 # DATABASES = {'default': dj_database_url.config(default='postgres://postgres:######@localhost/cardealer_db')}
