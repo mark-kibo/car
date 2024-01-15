a55db1299c9bb8f26ffc6eb493936a18cc7c7bbc
mark kibocha <63925315+mark-kibo@users.noreply.github.com>
2024-01-16 01:33:14 +0300

    Update settings.py

--- cardealer/settings.py
+++ cardealer/settings.py
@@ -113,7 +113,7 @@ WSGI_APPLICATION = 'cardealer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '../db.sqlite3',
     }
 }
 
